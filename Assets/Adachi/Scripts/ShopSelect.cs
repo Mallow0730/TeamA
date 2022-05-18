875d23fd965913f4c8d7af2794aa0e6999766411
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-05-18 15:24:48 +0900

    UI制作中

---
+++ Assets/Adachi/Scripts/ShopSelect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopSelect : MonoBehaviour
+{
+    /// <summary>メインカメラ</summary>
+    [SerializeField]
+    [Header("メインカメラ")]
+    Camera _mainCamera;
+
+    /// <summary>サブカメラ</summary>
+    [SerializeField]
+    [Header("サブカメラ")]
+    Camera _subCamera;
+
+    /// <summary>武器のショップ画面</summary>
+    [SerializeField]
+    [Header("武器のショップ画面")]
+    Canvas _weaponCanvas;
+
+
+
+    void Awake()
+    {
+        //_mainCamera = GetComponent<Camera>();
+        //_subCamera = GetComponent<Camera>();
+    }
+
+
+    /// <summary>左の<のButtonを押したらカメラを切り替える</summary>
+    public void LeftSelect()
+    {
+
+        _mainCamera.gameObject.SetActive(false);
+        _subCamera.gameObject.SetActive(true);
+    }
+
+    /// <summary>右の>のButtonを押したらカメラを切り替える</summary>
+    public void Right()
+    {
+        _mainCamera.gameObject.SetActive(true);
+        _subCamera.gameObject.SetActive(false);
+    }
+}
