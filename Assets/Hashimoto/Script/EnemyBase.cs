2605ff3984758aaf28956a6710824f27b7039545
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2022-06-29 18:56:13 +0900

    バトルシーン（ほとんど）完成

---
+++ Assets/Hashimoto/Script/EnemyBase.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyBase : MonoBehaviour
+{
+    /// <summary>ゲット出来るコイン量</summary>
+    public int GetCoin => _getCoin;
+
+    /// <summary>ゲット出来る経験値量</summary>
+    public int GetExp => _getExp;
+
+    /// <summary>UIのスライダー</summary>
+    public Slider Slider => _slider;
+
+    /// <summary>UIのキャンバス</summary>
+    public Canvas Canvas => _canvas;
+
+    /// <summary>武器の攻撃力</summary>
+    public int Attack => _attack;
+
+    /// <summary>敵の体力</summary>
+    public int EnemyHP => _enemyHP;
+
+    /// <summar>現在の体力</summary>
+    public int CurrentHP => _currentHP;
+
+    /// <summary>EnemyのHP</summary>
+    [SerializeField]
+    [Header("敵のHP")]
+    int _enemyHP;
+
+    /// <summary>現在の体力</summary>
+    int _currentHP;
+
+
+    /// <summary>ゲット出来るコイン量</summary>
+    [SerializeField]
+    [Header("ゲット出来るコイン量")] 
+    int _getCoin;
+
+    /// <summary>ゲット出来る経験値量</summary>
+    [SerializeField]
+    [Header("ゲット出来る経験値量")] 
+    int _getExp;
+
+    /// <summary>UIのスライダー</summary>
+    [SerializeField]
+    [Header("UIのスライダー")] 
+    Slider _slider;
+
+    /// <summary>UIのキャンバス</summary>
+    [SerializeField]
+    [Header("UIのキャンバス")] 
+    Canvas _canvas;
+
+    /// <summary>武器の攻撃力</summary>
+    [SerializeField]
+    [Header("武器の攻撃力")] 
+    int _attack;
+
+    protected virtual void Start()
+    {
+        Slider.maxValue = EnemyHP;
+        Slider.value = EnemyHP;
+        _currentHP = _enemyHP;
+        WeaponManager.Instance.WeaponAttack(_attack);
+    }
+    protected virtual void Update()
+    {
+        
+        Canvas.transform.rotation = Camera.main.transform.rotation;
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapon")
+        {
+            EnemyDamege();
+        }
+    }
+    protected virtual void EnemyDamege()
+    {
+        _enemyHP -= Attack;
+        Slider.value = EnemyHP;
+        if (EnemyHP <= 0)
+        {
+            _getCoin = Random.Range(1, 10);
+            _getExp = Random.Range(20, 50);
+            PlayerPrefs.SetInt("COINSCORE", +GetCoin);
+            PlayerPrefs.SetInt("EXPSCORE", +GetExp);
+            Destroy(this.gameObject);
+            //this.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Hashimoto/Script/GameManager.cs
+++ Assets/Hashimoto/Script/GameManager.cs
@@ -7,10 +7,10 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     public static GameManager instance = null;
 
     /// <summary>コインの合計値</summary>
-    public int Coin { get => _coin; set => _coin = value; }//コインの合計値
+    public int Coin => _coin;//コインの合計値
 
     /// <summary>コインの合計値</summary>
-    public int Exp { get => _exp; set => _exp = value; }//経験値の合計値
+    public int Exp => _exp;//経験値の合計値
 
 
     /// <summary>コインの合計値</summary>
@@ -20,28 +20,16 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     /// <summary>経験値の合計値</summary>
     [Header("経験値の合計値")]
     int _exp;
-
     
-    private void Awake()
-    {
-        //PlayerPrefs.DeleteAll();
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
-    }
     void Start()
     {
         
         PlayerPrefs.DeleteAll();
-        Coin = PlayerPrefs.GetInt("COINSCORE");
-        Exp =  PlayerPrefs.GetInt("EXPSCORE");
+        _coin = PlayerPrefs.GetInt("COINSCORE");
+        _exp =  PlayerPrefs.GetInt("EXPSCORE");
         Debug.Log(Coin);
         Debug.Log(Exp);
     }
+    public int GetCoin(int coin) => _coin = coin;
+    public int GetExp(int exp) => _exp = exp;
 }
