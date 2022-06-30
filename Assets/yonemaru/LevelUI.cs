4ae0b30fe2b2e838525ff509fd0b3c14323ee613
DESKTOP-VLN4SUU\vantan <yonemaru15@gmail.com>
2022-06-30 11:49:48 +0900

    レべルシーン

---
+++ Assets/yonemaru/LevelUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUI : MonoBehaviour
+{
+    public Text TextFrame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
