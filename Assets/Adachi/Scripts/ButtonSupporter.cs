7c240e3c97b994fb25b124ccd177fd968f56c4a8
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-07-14 17:41:30 +0900

    UIManager制作中

---
+++ Assets/Adachi/Scripts/ButtonSupporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSupporter : MonoBehaviour
+{
+    [SerializeField]
+    [Header("次に行きたい店の種類")]
+    BoothType _boothType;
+
+    [SerializeField]
+    [Header("次に行きたいUIの位置の種類")]
+    UIType _uiType;
+
+    public void NextMenu() => ShopUIManager.Instance.NextMenu(_boothType,_uiType);
+
+    public void BackMenu() => ShopUIManager.Instance.BackMenu();
+}
--- Assets/Adachi/Scripts/ShopUIManager.cs
+++ Assets/Adachi/Scripts/ShopUIManager.cs
@@ -6,9 +6,12 @@ using System.Linq;
 
 public class ShopUIManager : SingletonMonoBehaviour<ShopUIManager>
 {
+    public BoothType NowBoothType => _nowBoothType;
+    public UIType NowUIType => _nowUIType;
+
     [SerializeField]
-    [Header("全てのボタン")]
-    List<AllButton> _allButtons = new List<AllButton>();
+    [Header("全てのUI")]
+    List<AllUI> _allUIs = new List<AllUI>();
 
     [SerializeField]
     [Header("所持金のテキスト")]
@@ -26,7 +29,11 @@ public class ShopUIManager : SingletonMonoBehaviour<ShopUIManager>
     [Header("アイテム説明のパネル")]
     Image _itemDescriptionPanel;
 
-    void Start()
+    BoothType _nowBoothType = BoothType.Home;
+    UIType _nowUIType = UIType.Select0;
+    const int ONE = 1;
+
+    void Start() 
     {
         _moneyText.text = 0.ToString();
         _moneyText.gameObject.SetActive(true);
@@ -35,44 +42,52 @@ public class ShopUIManager : SingletonMonoBehaviour<ShopUIManager>
 
     /// <summary>ショップメニューを表示</summary>
     public void NextMenu(BoothType boothType, UIType uiType)
-    {           
-        _allButtons.Where(x => x.BoothType == boothType && x.UiType == uiType).ToList().ForEach(x => x.SetActive(true));
-        _allButtons.Where(x => x.UiType == uiType--).ToList().ForEach(x => x.SetActive(false));
+    {       
+        //次のUIを表示する
+        _allUIs.Where(x => x.BoothType == boothType && x.UiType == uiType).ToList().ForEach(x => x.SetActive(true));
+        //前のUIを非表示する
+        _allUIs.Where(x => x.UiType == uiType - ONE).ToList().ForEach(x => x.SetActive(false));
+        //現在の状態を保存
+        _nowBoothType = boothType;
+        _nowUIType = uiType;
     }
 
     /// <summary>戻る</summary>
-    public void BackMenu(BoothType boothType, UIType uiType)
-    {      
-        _allButtons.Where(x => x.BoothType == boothType && x.UiType == uiType).ToList().ForEach(x => x.SetActive(true));
-        _allButtons.Where(x => x.UiType == uiType++).ToList().ForEach(x => x.SetActive(false));
+    public void BackMenu()
+    {
+        //今のUIを非表示する
+        _allUIs.Where(x => x.BoothType == _nowBoothType && x.UiType == _nowUIType).ToList().ForEach(x => x.SetActive(false));
+        if (_nowBoothType == BoothType.ShopSell) _nowBoothType = BoothType.ShopBuy;
+        //次のUIを表示
+        _allUIs.Where(x => x.BoothType == _nowBoothType - ONE && x.UiType == _nowUIType - ONE).ToList().ForEach(x => x.SetActive(true));
     }
 
     [System.Serializable]
-    public class AllButton
+    public class AllUI
     {
         public BoothType BoothType => _boothType;
         public UIType UiType => _uiType;
-        public Button Button => _button;
+        public GameObject UI => _ui;
 
         [SerializeField]
-        [Header("名前")]
+        [Tooltip("名前")]
         string _name;
 
         [SerializeField]
-        [Header("店の種類")]
+        [Tooltip("店の種類")]
         BoothType _boothType;
 
         [SerializeField]
-        [Header("UIの位置の種類")]
+        [Tooltip("UIの位置の種類")]
         UIType _uiType;
 
         [SerializeField]
-        [Header("ボタン")]
-        Button _button;
+        [Tooltip("UI")]
+        GameObject _ui;
 
         public void SetActive(bool set)
         {
-            _button.gameObject.SetActive(set);
+            _ui.gameObject.SetActive(set);
         }
     }
 }
